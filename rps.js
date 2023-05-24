dd513e9f10613fcbdbf145e9db3b6fa7949bb94d
Joshua Brinson <joshbrin2000@gmail.com>
2023-05-24 13:36:15 -0230

    finished rps.js

--- rps.js
+++ rps.js
@@ -15,37 +15,53 @@ function getComputerChoice(){
 function singleRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase()
     if (playerSelection === computerSelection){
-        return "Tie!";
+        console.log("Game Round: Tie!")
+        return 0;
     }
     else if ((playerSelection === "rock" && computerSelection === "scissors") 
     || (playerSelection === "paper" && computerSelection === "rock") 
     || (playerSelection === "scissors" && computerSelection === "paper")){
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
+        console.log(`You Win This Round! ${playerSelection} beats ${computerSelection}`)
+        return 1;
     }
     else {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        console.log(`You Lose This Round! ${playerSelection} loses to ${computerSelection}`)
+        return -1;
     }
 }
 
 function game(){
     rounds = 5;
     input = "";
+    sum = 0;
     checkArray = ["rock", "paper", "scissors"]
     while (rounds-- > 0){
         while (!input)
         {
-            input = prompt("rock, paper, or scissors?")
+            input = prompt("Rock, Paper, or Scissors?")
             input = input.toLowerCase()
             if (checkArray.includes(input)){
                 continue;
             }
             else{
-                console.log("Please input a valid choice");
+                console.log("Please input a valid choice...");
                 input = "";
             }
         }
+        compGen = getComputerChoice();
+        result = singleRound(input, compGen);
+        sum += result;
+        input = "";
     }
-    
+    if (sum > 0){
+        console.log("You Win The Game!")
+    }
+    else if (sum < 0){
+        console.log("You Lose The Game!")
+    }
+    else{
+        console.log("You Tied With The Computer!")
+    }
 }
 
 game()
